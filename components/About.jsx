8094c6dcd97920f1e626cddf7113007921ed0a47
Atharva <iamatharvjaiswal@gmail.com>
2025-02-01 14:31:05 +0530

    v-2-2

--- components/About.jsx
+++ components/About.jsx
@@ -32,7 +32,7 @@ const About = ({isDarkMode}) => {
             whileInView={{opacity:1}}
             transition={{duration:0.8}}
             
-            className='flex w-full flex-col lg:flex-row items-center gap-20 my-20'>
+            className='flex w-full flex-col lg:flex-row items-center gap-20 my-10'>
                 <motion.div
                 initial={{opacity:0,scale:0.9}}
                 whileInView={{opacity:1,scale:1}}
